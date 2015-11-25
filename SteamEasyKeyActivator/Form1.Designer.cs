04b24686f2078fbafce8f6c86930fb56e94e48d3
Arda Özkal <arda.ozkal@studentpartner.com>
2015-11-25 14:17:53 +0200

    SEKA FW v1

---
+++ SteamEasyKeyActivator/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SteamEasyKeyActivator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 29);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(334, 195);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(334, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Please start steam first.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Your codes: (one per line)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(347, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "By ardaozkal. You are responsible for anything you do with this software.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 277);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Steam Easy Key Activator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SteamEasyKeyActivator/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteamEasyKeyActivator
+{
+    public partial class Form1 : Form
+    {
+        //titles from http://translation.steampowered.com/Us_And_Them.php?token_key_ID=54280, you need to be a steam translator to access the link
+        List<string> activationtitles = new List<string> { "Product Activation", "Aktivace produktu", "Produktaktivering", "Productactivering", "Tuotteen aktivointi", "Activation de produit", "Produktaktivierung", "Termékaktiválás", "Attivazione del prodotto", "製品を有効化", "제품 등록", "Veiviser for aktivering av produkt", "Aktywacja produktu", "Ativação de produto", "Activare Produs", "Активация продукта", "Activación de producto", "Produktaktiveringsguide", "产品激活", "產品啟動", "การเปิดใช้ผลิตภัณฑ์", "Ativação de produto", "Активиране на продукт", "Ενεργοποίηση προϊόντος", "Ürün Etkinleştirme", "Активація продукту", "Kích hoạt sản phẩm" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName("Steam"); //Checks if steam is running
+            if (pname.Length == 0)
+            {
+                button1.Enabled = false;
+                button1.Text = "Please start steam first.";
+            }
+            else
+            {
+                button1.Enabled = true;
+                button1.Text = "Start Activating";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (string code in richTextBox1.Text.Split(new[] { '\r', '\n' }))
+            {
+                Process.Start("steam://open/activateproduct"); //opens the activate product window
+                Thread.Sleep(100);
+                SendKeys.SendWait("{ENTER}"); //Clicks the next button
+                Thread.Sleep(100);
+                SendKeys.SendWait("{ENTER}"); //Clicks the I agree button
+                Thread.Sleep(100);
+                SendKeys.SendWait(code); //Types down the key
+                Thread.Sleep(100);
+                SendKeys.SendWait("{ENTER}"); //Clicks the activate button
+                Thread.Sleep(500);
+                var currentwindowtitle = GetActiveWindowTitle();
+                while (currentwindowtitle.StartsWith("Steam ")) //It shows steam - working, I'd do "Steam -" but some languages use different characters, so this is better than spaghetti code
+                {
+                    currentwindowtitle = GetActiveWindowTitle();
+                    Thread.Sleep(100);
+                }
+                while (activationtitles.Contains(currentwindowtitle))
+                {
+                    SendKeys.SendWait("{ESC}"); //Exits the activate product window
+                    Thread.Sleep(100);
+                    currentwindowtitle = GetActiveWindowTitle();
+                }
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        { //http://stackoverflow.com/questions/115868/how-do-i-get-the-title-of-the-current-active-window-using-c
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/ardaozkal/Steam_Easy_Key_Activator");
+        }
+    }
+}
